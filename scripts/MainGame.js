bf834d69690e4955f481c1b2e7a64b81324ff6fc
AndrewTilt9 <T1506367@student.southstaffs.ac.uk>
2017-05-26 10:16:42 +0100

    Update MainGame.js

--- scripts/MainGame.js
+++ scripts/MainGame.js
@@ -1,14 +1,29 @@
 BasicGame.Game = function (game) {};
+//Graphical Object
+var ship;
 
 BasicGame.Game.prototype = {
 
-	create: function () {
-       
-	},
+create: function () {
+//Specifying the physics game engine to ARCADE
+this.physics.startSystem(Phaser.Physics.ARCADE);
+//Adding the starfield, logo onto the screen
+this.starfield = this.add.tileSprite(0, 0, 800, 600, 'starfield');
+ //Adding the ship onto the screen, set the physics and the
+boundarys
+14 ship = this.add.sprite((this.world.width / 2), this.world.height -
+50, 'ship');
+ship.anchor.setTo(0.5,0);
+this.physics.enable(ship, Phaser.Physics.ARCADE);
+ ship.body.collideWorldBounds = true;
+ },
+ update: function () {
 
-	update: function () {
-		//execute 'createUfo','createLife','moveShip','collisionDetection' function
-		
-	}
+ //execute 'createUfo','createLife','moveShip','collisionDetection'
+function
+
+
+}
+
 
 };
